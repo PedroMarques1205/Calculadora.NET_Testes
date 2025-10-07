80b2575f806f69bbc6040242140aa1b44ba8798b
Juliana Parreiras <129118808+juulipana@users.noreply.github.com>
2025-10-07 17:47:57 -0300

    Add docs

--- Code/ConsoleApp1/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Code/ConsoleApp1/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d63996e3f1f39d30fbfba31e66d91ba714d2eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a7a539f922c8f488aa8308f75dbcc2a639180aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
